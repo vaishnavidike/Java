16f11a648b66f892880befb75e4ceaf98153c22e
Vaishnavi Dike <dikevaishanavi351@gmail.com>
2026-01-22 22:29:05 +0530

    Add String2ReverseString class to reverse a string
    Implement a Java program to reverse a string using StringBuilder.

---
+++ String2ReverseString.java
@@ -0,0 +1,13 @@
+public class String2ReverseString {
+    public static void main(String[] args){
+
+        //Reverse a string one line code
+
+        String str="Hello";
+        // StringBuilder rev=new StringBuilder(str);
+        // rev.reverse();
+        System.out.println(new StringBuilder(str).reverse());
+    }
+    }
+    
+
